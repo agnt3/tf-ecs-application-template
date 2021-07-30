17e966318a41e0b6162cedf37b8bed2c2a1dabc5
leonardocordeiro <leonardobcordeirodev@gmail.com>
2021-07-30 14:28:24 -0300

    deployment and dockerize functions on deploy.sh

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 
+COMMIT_HASH=$1
+
 deploy() {
-  export TF_VAR_commit_hash=$1
-  echo "Commit Hash: $TF_VAR_commit_hash"
+  echo "Starting Deployment..."
+  echo "Commit Hash: $COMMIT_HASH"
+
+  export TF_VAR_commit_hash=$COMMIT_HASH
   terraform apply -var-file="production.tfvars" -auto-approve
 }
 
-deploy $1
+dockerize() {
+  IMAGE_NAME=leonardocordeiro/ecs-example-app:$COMMIT_HASH
+
+  docker build -t $IMAGE_NAME $(dirname $PWD)
+  docker push $IMAGE_NAME
+}
+
+case $2 in
+  "deploy")
+      deploy;;
+  "dockerize")
+      dockerize;;
+esac
